5e94aa9c07d3a27f729bdadfbd99ca0c20e35f7c
Tobias <tobias.bp2004@gmail.com>
2026-01-23 14:10:34 -0300

    fix: register

--- src/server.ts
+++ src/server.ts
@@ -58,16 +58,22 @@ app.register(fastifySwagger, {
 	transform: jsonSchemaTransform,
 });
 
-app.register(async (instance) => {
-	const ScalarApiReference = await import('@scalar/fastify-api-reference');
-	instance.register(ScalarApiReference.default || ScalarApiReference, {
-		routePrefix: '/docs',
+if (process.env.NODE_ENV !== 'production') {
+	app.register(async (instance) => {
+		const ScalarApiReference = await import('@scalar/fastify-api-reference');
+		instance.register(ScalarApiReference.default || ScalarApiReference, {
+			routePrefix: '/docs',
+		});
 	});
-});
+}
 
 app.register(routes);
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-	console.log('HTTP server running on http://localhost:3333!');
-	console.log('Docs available at http://localhost:3333/docs');
-});
+if (require.main === module) {
+	app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+		console.log('HTTP server running on http://localhost:3333!');
+		console.log('Docs available at http://localhost:3333/docs');
+	});
+}
+
+export { app };
