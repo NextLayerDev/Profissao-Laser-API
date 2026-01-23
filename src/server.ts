0a4890b32b57ca111b108faf7b0244a8b37c9797
Tobias <tobias.bp2004@gmail.com>
2026-01-23 11:19:05 -0300

    fix: deploy settings

--- src/server.ts
+++ src/server.ts
@@ -71,3 +71,5 @@ app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
 	console.log('HTTP server running on http://localhost:3333!');
 	console.log('Docs available at http://localhost:3333/docs');
 });
+
+export default app;
