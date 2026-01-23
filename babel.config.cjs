5b6b73ebe19fc5f1ba7b3250edde6d2b4a9ef159
Tobias <tobias.bp2004@gmail.com>
2026-01-23 10:45:36 -0300

    fix: deploy settings

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,29 +1,29 @@
 const plugins = [
-  [
-    'module-resolver',
-    {
-      root: ['./src'],
-      alias: {
-        '@': './src',
-      },
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-  ],
-  ['babel-plugin-add-import-extension', { extension: 'js' }],
+	[
+		'module-resolver',
+		{
+			root: ['./src'],
+			alias: {
+				'@': './src',
+			},
+			extensions: ['.js', '.jsx', '.ts', '.tsx'],
+		},
+	],
+	['babel-plugin-add-import-extension', { extension: 'js' }],
 ];
 
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-        modules: false,
-      },
-    ],
-    '@babel/preset-typescript',
-  ],
-  plugins
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 'current',
+				},
+				modules: false,
+			},
+		],
+		'@babel/preset-typescript',
+	],
+	plugins,
 };
