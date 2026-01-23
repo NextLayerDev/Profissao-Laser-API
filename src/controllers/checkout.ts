d5559608c2056a7a29706a9c9674d3d1c0aaf1e9
tobsBP <tobias.bp2004@gmail.com>
2026-01-23 18:07:44 -0300

    fix: error type

--- src/controllers/checkout.ts
+++ src/controllers/checkout.ts
@@ -9,7 +9,7 @@ export const createCheckoutSessionController = async (
 ) => {
 	try {
 		const { priceId, successUrl, cancelUrl } = request.body as CheckoutBody;
-		const user = request.user as User;
+		const user = request.body as User;
 
 		if (!user || !user.email) {
 			return reply.status(401).send({ message: 'User email not found' });
