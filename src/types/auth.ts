603f716a7e5f29ad74e5b5ef286b325dc84d09cb
tobsBP <tobias.bp2004@gmail.com>
2026-01-24 15:24:20 -0300

    fix: create user

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -11,7 +11,6 @@ export const registerUserSchema = z.object({
 	name: z.string().min(2, 'Name must be at least 2 characters long'),
 	password: z.string().min(6, 'Password must be at least 6 characters long'),
 	Permissions: z.number().nullable(),
-	created_at: z.string(),
 });
 
 export const registerCustomerSchema = z.object({
