906c9379c5132a7654b429a2ecac21de9f977b76
tobsBP <tobias.bp2004@gmail.com>
2026-03-03 20:08:16 -0300

    fix: cors problem

--- src/server.ts
+++ src/server.ts
@@ -20,8 +20,9 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(multipart, { limits: { fileSize: 1000 * 1024 * 1024 } }); // 500 MB
 
 app.register(fastifyCors, {
-	origin: true,
+	origin: 'https://profissao-laser-profissao-laser-front.1nwz76.easypanel.host',
 	methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
 if (!process.env.JWT_SECRET) {
