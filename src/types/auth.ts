b85c7da246f0f2d0fbca81be1f541289e22f7752
Tobias <tobias.bp2004@gmail.com>
2026-04-09 09:12:06 -0300

    fix: email to lower

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,20 +1,20 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-	email: z.email('Invalid email'),
+	email: z.email('Invalid email').toLowerCase(),
 	password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
 
 export const registerUserSchema = z.object({
 	role: z.enum(['Super Admin', 'Financial', 'admin', 'colaborador']),
-	email: z.email('Invalid email'),
+	email: z.email('Invalid email').toLowerCase(),
 	name: z.string().min(2, 'Name must be at least 2 characters long'),
 	password: z.string().min(6, 'Password must be at least 6 characters long'),
 	Permissions: z.number().nullable(),
 });
 
 export const registerCustomerSchema = z.object({
-	email: z.email('Invalid email'),
+	email: z.email('Invalid email').toLowerCase(),
 	name: z.string().min(2, 'Name must be at least 2 characters long'),
 	password: z.string().min(6, 'Password must be at least 6 characters long'),
 	phone: z.string().nullish(),
